69c0658c27e5c1fbbc00ef25b86e5fa0b71ae062
Matt Stratton <matt.stratton@gmail.com>
2018-11-09 19:16:33 -0800

    Add sorting and filtering to all other table views
    Fixes #38
    
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,10 +5,29 @@ require( 'datatables.net-bs4' )( window, $ );
 // Set using https://datatables.net/
 
 $(document).ready( function () {
-    console.log($);
     $('#eventTable').DataTable( {
         "columnDefs": [
             { "orderable": false, "targets": 2 }
+          ]
+    });
+    $('#abstractTable').DataTable( {
+        "columnDefs": [
+            { "orderable": false, "targets": 2 }
+          ]
+    });
+    $('#dutonianTable').DataTable( {
+        "columnDefs": [
+            { "orderable": false, "targets": 1 }
+          ]
+    });
+    $('#proposalTable').DataTable( {
+        "columnDefs": [
+            { "orderable": false, "targets": 4 }
+          ]
+    });
+    $('#sponsorshipTable').DataTable( {
+        "columnDefs": [
+            { "orderable": false, "targets": 3 }
           ]
     });
 } );
